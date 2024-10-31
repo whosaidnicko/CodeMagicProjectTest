8de1f853172d8cc309f1af26c56a7ebdd52fe7f9
whosaidnicko <nicu161299@gmail.com>
2024-10-31 12:55:17 +0200

    Initial Commit

---
+++ CodeMagicProjectTest/CodeMagicProjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeMagicProjectTestApp.swift
+//  CodeMagicProjectTest
+//
+//  Created by Nicolae Chivriga on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CodeMagicProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeMagicProjectTest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CodeMagicProjectTest
+//
+//  Created by Nicolae Chivriga on 31/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
